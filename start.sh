bb0bf2574031a222188fda4011fe03a34a13cd14
ymx <ymx01015850@alibaba-inc.com>
2022-08-31 14:50:15 +0800

    结构优化

--- start.sh
+++ start.sh
@@ -6,10 +6,6 @@
 isUpdate=1
 #jdk 目录 例如: c/user/deskTop/jdk1.8/bin 请手动填写
 javaPath=c/user/deskTop/jdk1.8/bin
-if [ x"$1" = x ]; then
-  echo "java path no cmd param!"
-  exit 1
-fi
 
 uNames=$(uname -s)
 osName=${uNames:0:4}
